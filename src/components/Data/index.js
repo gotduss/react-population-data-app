e3913ca3428534e09264d71398ddedca47573747
Gauthier <144640575+GauthierBCC@users.noreply.github.com>
2023-12-22 13:27:56 +1000

    refactor chart options

--- src/components/Data/index.js
+++ src/components/Data/index.js
@@ -3,3 +3,40 @@ export const POP_API_URL = process.env.REACT_APP_POP_API_URL;
 
 // List of excluded country codes (combined countries)
 export const excludedCountryCodes = ['ZH', 'ZI', '1A', 'S3', 'B8', 'V2', 'Z4', '4E', 'T4', 'XC', 'Z7', '7E', 'T7', 'EU', 'F1', 'XE', 'XD', 'XF', 'ZT', 'XH', 'XI', 'XG', 'V3', 'ZJ', 'XJ', 'T2', 'XL', 'XO', 'XM', 'XN', 'ZQ', 'XQ', 'T3', 'XP', 'XU', 'XY', 'OE', 'S4', 'S2', 'V4', 'V1', 'S1', '8S', 'T5', 'ZG', 'ZF', 'T6', 'XT', '1W'];
+
+// chartOptions
+export const getChartOptions = (titleText, type, xAxisData, seriesData) => {
+  return {
+    title: {
+      text: titleText || '',
+    },
+    xAxis: {
+      type: 'category',
+      data: xAxisData || [],
+      axisLabel: {
+        interval: 0,
+        rotate: 45,
+      },
+    },
+    yAxis: {
+      type: 'value',
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
+      formatter: '{b}: {c}',
+    },
+    series: [
+      {
+        name: 'Population',
+        type: type || 'bar',
+        data: seriesData || [],
+        animation: true,
+        animationDuration: 1000,
+        animationEasing: 'cubicOut',
+      },
+    ],
+  };
+};
--- src/components/WorldPopulationChart/index.js
+++ src/components/WorldPopulationChart/index.js
@@ -1,6 +1,6 @@
 /* Imports */
 import React, { useState, useEffect } from 'react';
-import { POP_API_URL, excludedCountryCodes } from '../Data';
+import { POP_API_URL, excludedCountryCodes, getChartOptions } from '../Data';
 import ReactEChart from 'echarts-for-react';
 
 /* WorldPopulationChart Component */
@@ -38,39 +38,12 @@ const WorldPopulationChart = () => {
     fetchWorldPopulation();
   }, [totalResults]);
 
-  const options = {
-    title: {
-      text: 'Top 10 Countries by Population (2022)',
-    },
-    xAxis: {
-      type: 'category',
-      data: countriesData.map((item) => item.country.value),
-      axisLabel: {
-        interval: 0, // Display all labels
-        rotate: 45, // Rotate labels for better readability
-      },
-    },
-    yAxis: {
-      type: 'value',
-    },
-    tooltip: {
-      trigger: 'axis', // Show tooltips on hover over the bars
-      axisPointer: {
-        type: 'shadow', // Display tooltips as a shadow under the bars
-      },
-      formatter: '{b}: {c}', // Display the country name and its value in the tooltip
-    },
-    series: [
-      {
-        name: 'Population',
-        type: 'bar',
-        data: countriesData.map((country) => country.value),
-        animation: true, // Enable animation
-        animationDuration: 1000, // Animation duration in milliseconds
-        animationEasing: 'cubicOut', // Animation easing function
-      },
-    ],
-  };
+  const options = getChartOptions(
+    'Top 10 Countries by Population (2022)',
+    'bar',
+    countriesData.map((item) => item.country.value),
+    countriesData.map((country) => country.value)
+  );
 
   return (
     <div className="WorldPopulationChart">
